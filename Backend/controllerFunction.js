f6a3f8a843c4973c2a09376894bed50c2e134da6
fleet2005 <100420vishalsrinivasan.gbkm@gmail.com>
2025-02-18 22:38:25 +0530

    Bcrypt added

--- Backend/controllerFunction.js
+++ Backend/controllerFunction.js
@@ -1,12 +1,13 @@
 const asyncHandler = require("express-async-handler");
 const signupmodel = require("./models/UserLoginSchema.js");
+const bcrypt = require("bcrypt")
 
 const signin = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
 
-    const document = await signupmodel.findOne({ email, password });
+    const document = await signupmodel.findOne({ email });
 
-    if (document) {
+    if (document && await bcrypt.compare(password, document.password)) {
         res.json({"sts":"true"});
     } else {
         res.status(400).json({"sts":"false"});
@@ -16,8 +17,25 @@ const signin = asyncHandler(async (req, res) => {
 
 const signup = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
+    const document = await signupmodel.findOne({email});
 
-    const newDocument = new signupmodel({ email, password });
+    if(document)
+    {
+        return res.status(400).json({"sts" : "User already exists"});
+    }
+
+    if (!password) 
+    {
+        return res.status(400).json({ "sts": "Password is required" });
+    }
+
+    console.log(req.body);
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    console.log(hashedPassword);
+
+    const newDocument = new signupmodel({ email: email, password: hashedPassword});
 
     await newDocument.save();
 
