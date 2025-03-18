3c10df57b105a813e84c1693fb79fdcb3a7a68ce
fleet2005 <100420vishalsrinivasan.gbkm@gmail.com>
2025-03-18 17:40:30 +0530

    Build fix 5.0

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
+    },
+  },
 })
